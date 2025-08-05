daf223e8261475c09d38d854fffef4e76b89839a
NurbekLDM <jumaboyevabarno80@gmail.com>
2025-08-05 23:36:06 +0500

    fixed 4

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require("dotenv").config()
 const express = require("express")
-const { RedisDB } = require('./lib/redis');
 const TelegramBot = require("node-telegram-bot-api")
 const redis = require("redis")
 const fs = require("fs-extra")
